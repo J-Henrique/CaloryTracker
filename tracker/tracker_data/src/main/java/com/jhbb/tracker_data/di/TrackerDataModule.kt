8a7162b99048104ba4b48d9230c841e2bc7b35cb
João Bortolotto <joao.bortolotto@willbank.com.br>
2022-12-12 09:52:02 -0300

    Setting up tracker API

---
+++ tracker/tracker_data/src/main/java/com/jhbb/tracker_data/di/TrackerDataModule.kt
@@ -0,0 +1,45 @@
+package com.jhbb.tracker_data.di
+
+import com.jhbb.tracker_data.remote.OpenFoodApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(OpenFoodApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenFoodApi(
+        retrofit: Retrofit
+    ): OpenFoodApi {
+        return retrofit.create(OpenFoodApi::class.java)
+    }
+}
