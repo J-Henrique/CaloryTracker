9b97c34ccc7832b4ed0bc45a82096f558ce392b9
João Bortolotto <joao.bortolotto@willbank.com.br>
2022-12-15 15:04:30 -0300

    Tracker viewmodel

---
+++ tracker/tracker_presentation/src/main/java/com/jhbb/tracker_presentation/tracker_overview/TrackerOverviewEvent.kt
@@ -0,0 +1,11 @@
+package com.jhbb.tracker_presentation.tracker_overview
+
+import com.jhbb.tracker_domain.model.TrackedFood
+
+sealed class TrackerOverviewEvent {
+    object OnNextDayClick: TrackerOverviewEvent()
+    object OnPreviousDayClick: TrackerOverviewEvent()
+    data class OnToggleMealClick(val meal: Meal): TrackerOverviewEvent()
+    data class OnDeleteTrackedFoodClick(val food: TrackedFood): TrackerOverviewEvent()
+    data class OnAddFoodClick(val meal: Meal): TrackerOverviewEvent()
+}
