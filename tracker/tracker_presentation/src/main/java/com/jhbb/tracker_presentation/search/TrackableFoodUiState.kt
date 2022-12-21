1feca92598e092bfd6352b6ec85d5cbb6ee6ade2
João Bortolotto <joao.bortolotto@willbank.com.br>
2022-12-21 16:45:41 -0300

    SearchViewModel.kt

---
+++ tracker/tracker_presentation/src/main/java/com/jhbb/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.jhbb.tracker_presentation.search
+
+import com.jhbb.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
