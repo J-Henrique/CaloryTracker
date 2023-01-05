0fd47416f513399a5da6f749b34668100c96ae62
João Henrique Buso Bortolotto <joao.bortolotto@willbank.com.br>
2023-01-05 14:34:27 -0300

    E2E tests

--- app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverviewE2E.kt
+++ app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverviewE2E.kt
@@ -1,25 +1,44 @@
 package com.plcoding.calorytracker
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import coil.annotation.ExperimentalCoilApi
+import com.google.common.truth.Truth.assertThat
 import com.jhbb.core.domain.model.ActivityLevel
 import com.jhbb.core.domain.model.Gender
 import com.jhbb.core.domain.model.GoalType
 import com.jhbb.core.domain.model.UserInfo
 import com.jhbb.core.domain.preferences.Preferences
 import com.jhbb.core.domain.use_case.FilterOutDigits
+import com.jhbb.tracker_domain.model.TrackableFood
 import com.jhbb.tracker_domain.use_case.*
+import com.jhbb.tracker_presentation.search.SearchScreen
 import com.jhbb.tracker_presentation.search.SearchViewModel
+import com.jhbb.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import com.jhbb.tracker_presentation.tracker_overview.TrackerOverviewViewModel
+import com.plcoding.calorytracker.navigation.Route
 import com.plcoding.calorytracker.repository.TrackerRepositoryFake
+import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.InternalPlatformDsl.toStr
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import kotlin.math.roundToInt
 
 @ExperimentalComposeUiApi
 @ExperimentalCoilApi
@@ -69,5 +88,146 @@ class TrackerOverviewE2E {
             trackerUseCases = trackerUseCases,
             filterOutDigits = FilterOutDigits()
         )
+        composeRule.setContent {
+            CaloryTrackerTheme {
+                val scaffoldState = rememberScaffoldState()
+                navController = rememberNavController()
+                Scaffold(modifier = Modifier.fillMaxSize(), scaffoldState = scaffoldState) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.TRACKER_OVERVIEW
+                    ) {
+                        composable(Route.TRACKER_OVERVIEW) {
+                            TrackerOverviewScreen(
+                                onNavigateToSearch = { mealName, day, month, year ->
+                                    navController.navigate(
+                                        Route.SEARCH +
+                                                "/$mealName" +
+                                                "/$day" +
+                                                "/$month" +
+                                                "/$year"
+                                    )
+                                },
+                                viewModel = trackerOverviewViewModel
+                            )
+                        }
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                },
+                            )
+                        ) {
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                },
+                                viewModel = searchViewModel
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun addBreakfast_appearsUnderBreakfast_nutrientsProperlyCalculated() {
+        repository.searchResults = listOf(
+            TrackableFood(
+                name = "banana",
+                imageUrl = null,
+                caloriesPer100g = 150,
+                proteinPer100g = 5,
+                carbsPer100g = 50,
+                fatPer100g = 1
+            )
+        )
+        val addedAmount = 150
+        val expectedCalories = (1.5f * 150).roundToInt()
+        val expectedCarbs = (1.5f * 50).roundToInt()
+        val expectedProtein = (1.5f * 5).roundToInt()
+        val expectedFat = (1.5f * 1).roundToInt()
+
+        composeRule
+            .onNodeWithText("Add Breakfast")
+            .assertDoesNotExist()
+        composeRule
+            .onNodeWithContentDescription("Breakfast")
+            .performClick()
+        composeRule
+            .onNodeWithText("Add Breakfast")
+            .assertIsDisplayed()
+        composeRule
+            .onNodeWithText("Add Breakfast")
+            .performClick()
+
+        assertThat(
+            navController.currentDestination
+                ?.route
+                ?.startsWith(Route.SEARCH)
+        ).isTrue()
+
+        composeRule
+            .onNodeWithTag("search_textfield")
+            .performTextInput("banana")
+        composeRule
+            .onNodeWithContentDescription("Search...")
+            .performClick()
+
+        composeRule.onRoot().printToLog("COMPOSE TREE")
+
+        composeRule
+            .onNodeWithText("Carbs")
+            .performClick()
+        composeRule
+            .onNodeWithContentDescription("Amount")
+            .performTextInput(addedAmount.toStr())
+        composeRule
+            .onNodeWithContentDescription("Track")
+            .performClick()
+
+        assertThat(
+            navController
+                .currentDestination
+                ?.route
+                ?.startsWith(Route.TRACKER_OVERVIEW)
+        )
+
+        composeRule
+            .onAllNodesWithText(expectedCarbs.toStr())
+            .onFirst()
+            .assertIsDisplayed()
+        composeRule
+            .onAllNodesWithText(expectedProtein.toStr())
+            .onFirst()
+            .assertIsDisplayed()
+        composeRule
+            .onAllNodesWithText(expectedFat.toStr())
+            .onFirst()
+            .assertIsDisplayed()
+        composeRule
+            .onAllNodesWithText(expectedCalories.toStr())
+            .onFirst()
+            .assertIsDisplayed()
     }
 }
